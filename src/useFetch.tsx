e04a2b39bc2603d362f99f27544e12e31955a3f6
Christian Cordeiro <christiancordeiro96@gmail.com>
2024-08-25 05:02:20 -0300

    remove api key

--- src/useFetch.tsx
+++ src/useFetch.tsx
@@ -4,14 +4,13 @@ export function useFetchData<T>(endpoint: string) {
   const [data, setData] = useState<T | null>(null)
   const [loading, setLoading] = useState(false)
 
-  const apiKey = import.meta.env.VITE_APP_API_KEY
   const maxResults = 40
 
   useEffect(() => {
     async function fetchData() {
       try {
         setLoading(true)
-        const url = `https://www.googleapis.com/books/v1/${endpoint}&maxResults=${maxResults}&key=${apiKey}`
+        const url = `https://www.googleapis.com/books/v1/${endpoint}&maxResults=${maxResults}`
         const response = await fetch(url)
         const json = await response.json()
         setLoading(false)
